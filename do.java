71a4c9db921b8643c3788bded8c71b62570babf6
Sasirekhamylsamy <sasirekhagokilavani0108@gmail.com>
2017-06-07 17:14:16 +0530

    Create do.java

---
+++ do.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class sas
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+System.out.println("enter the number");
+int n=scan.nextInt();
+if(n%2==0)
+{
+System.out.println("the number is even number");
+}
+else
+{
+System.out.println("the number is odd number");
+}
+}
+}
